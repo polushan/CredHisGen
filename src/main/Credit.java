a356571fcfdfdd04a3a90c1f8f76a07e5126b53c
polushan <polushan@yandex.ru>
2018-02-02 11:20:31 +0400

    Initial commit

---
+++ src/main/Credit.java
@@ -0,0 +1,68 @@
+package main;
+
+public class Credit {
+    private long id;
+    private double sum;
+    private boolean isClosed;
+    private boolean isOverdue;
+    private long borrower;
+
+    public Credit(long id, double sum, boolean isClosed, boolean isOverdue, long borrower) {
+        this.id = id;
+        this.sum = sum;
+        this.isClosed = isClosed;
+        this.isOverdue = isOverdue;
+        this.borrower = borrower;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    public void setOverdue(boolean overdue) {
+        isOverdue = overdue;
+    }
+
+    public void setBorrower(long borrower) {
+        this.borrower = borrower;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public boolean isOverdue() {
+        return isOverdue;
+    }
+
+    public long getBorrower() {
+        return borrower;
+    }
+
+    @Override
+    public String toString() {
+        return "Credit{" +
+                "id=" + id +
+                ", sum=" + sum +
+                ", isClosed=" + isClosed +
+                ", isOverdue=" + isOverdue +
+                ", borrower=" + borrower +
+                '}';
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,68 @@
+package main;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Map<Integer, List<Credit>> creditHistory = new HashMap<>(10);
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            creditHistory.put(i, new ArrayList<>());
+        }
+        for (int i = 0; i < 160; i++) {
+            Credit credit = null;
+            int borrower = i % 10;
+            if ((i / 10) % 2 == 1 && i > 20) {
+                List<Credit> credits = creditHistory.get(borrower);
+                int creditCount = credits.size();
+                credit = credits.get(rand.nextInt(creditCount) );
+                credit.setClosed(true);
+                credit.setOverdue(rand.nextBoolean());
+            } else {
+                long id = rand.nextLong();
+                credit = new Credit(rand.nextLong(), rand.nextInt(1_000_000),
+                        false, false, borrower + 1);
+                creditHistory.get(borrower).add(credit);
+            }
+            //Thread.sleep(3 * 60 * 1000);
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost("http://localhost:8080/api/add/credit");
+
+            List<NameValuePair> arguments = new ArrayList<>(3);
+            arguments.add(new BasicNameValuePair("apikey", "secretapikey"));
+            arguments.add(new BasicNameValuePair("id", String.valueOf(credit.getId())));
+            arguments.add(new BasicNameValuePair("sum", String.valueOf(credit.getSum())));
+            arguments.add(new BasicNameValuePair("isclosed", String.valueOf(credit.isClosed())));
+            arguments.add(new BasicNameValuePair("isoverdue", String.valueOf(credit.isOverdue())));
+            arguments.add(new BasicNameValuePair("borrower", String.valueOf(credit.getBorrower())));
+
+            try {
+                post.setEntity(new UrlEncodedFormEntity(arguments));
+                HttpResponse response = client.execute(post);
+
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //Thread.sleep( 100);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("=========" + i + "=========");
+            List<Credit> creditsList = creditHistory.get(i);
+            for (Credit credit: creditsList) {
+                System.out.println(credit);
+            }
+            System.out.println("===================");
+        }
+    }
+}
